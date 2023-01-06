6656b47aec67b0e545d032797452c45ec3528632
nucklass <60252281+nucklass@users.noreply.github.com>
2023-01-05 19:15:45 -0800

    add build

--- src/jaurs.jl
+++ src/jaurs.jl
@@ -1,5 +1,9 @@
 module jaurs
 
-# Write your package code here.
+using Comonicon
 
+@cast mycmd1(arg; option="Sam") = println("cmd1: arg=", arg, "option=", option)
+@cast mycmd2(arg; option="Sam") = println("cmd2: arg=", arg, "option=", option)
+
+@main
 end
